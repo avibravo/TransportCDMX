d693dcc71ad85b02dc1e107d74130d7e8949b10e
avibravo <77843217+avibravo@users.noreply.github.com>
2021-03-12 03:00:50 -0600

    Initial Commit

---
+++ Shared/transportcdmxApp.swift
@@ -0,0 +1,17 @@
+//
+//  transportcdmxApp.swift
+//  Shared
+//
+//  Created by Javier Bravo on 3/12/21.
+//
+
+import SwiftUI
+
+@main
+struct transportcdmxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
